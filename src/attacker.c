643696e851a9876e3dfe6823634ebb7b69317e8d
G. Queiroz <gabrieljvnq@gmail.com>
2022-03-27 14:41:24 -0300

    Done

--- src/attacker.c
+++ src/attacker.c
@@ -12,18 +12,32 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+#ifndef ITEM_H
+position_t get_item_position(Item item);
+#endif
+
+struct _attack_spy {
+  Item item;
+  size_t number_uses;
+};
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_attacker_strategy(
-    position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+    position_t attacker_pos, Spy defender_spy) {
+  struct _attack_spy *spy = (struct _attack_spy*) defender_spy;
+  position_t defender_pos = get_item_position(spy->item);
+  direction_t ans = DIR_RIGHT;
+  
+  int delta = attacker_pos.i - defender_pos.i;
+  if (delta < 0) delta *= -1;
+  if (delta < 2) {
+    ans.i = 1;
+  }
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  return ans;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -12,18 +12,38 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+#ifndef ITEM_H
+position_t get_item_position(Item item);
+#endif
+
+struct _defender_spy {
+  Item item;
+  size_t number_uses;
+};
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+    position_t defender_pos, Spy attacker_spy) {
+  struct _defender_spy *spy = (struct _defender_spy*) attacker_spy;
+  position_t attacker_pos = get_item_position(spy->item);
+  direction_t ans = DIR_STAY;
+  
+  if (attacker_pos.i > defender_pos.i) {
+    ans.i = 1;
+  } else if (attacker_pos.i < defender_pos.i) {
+    ans.i = -1;
+  }
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  if (attacker_pos.j > defender_pos.j) {
+    ans.j = 1;
+  } else if (attacker_pos.j < defender_pos.j) {
+    ans.j = -1;
+  }
+
+  return ans;
 }
 
 /*----------------------------------------------------------------------------*/
